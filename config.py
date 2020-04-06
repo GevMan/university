e6871ab3d577b17207637f7482d5e5beebeaf567
Gevorg <gevman97@gmail.com>
2020-04-06 19:19:38 +0400

    first commit

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Configuration(object):
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI='mysql://khjwn643kklif6pm:gzsaa2s8ptpt7l5y@u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yk4hhipffs6x22ij'
+    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://khjwn643kklif6pm:gzsaa2s8ptpt7l5y@u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yk4hhipffs6x22ij'
     SQLALCHEMY_POOL_RECYCLE = 60
 
 
